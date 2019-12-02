d68112c35d58fc61e6abd2b506b16f903fd25bdc
Marcelo Hosan <mhosan@gmail.com>
2019-12-02 15:04:06 -0300

    primero

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,19 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'formularios';
+  form: FormGroup;
+
+  constructor(){
+    this.form=new FormGroup({
+      nombreFC : new FormControl('', Validators.required),
+      apellidoFC : new FormControl('', Validators.required),
+      correoFC : new FormControl('', [Validators.required, Validators.email])
+    });
+  }
+  
+  submitForm(){
+    console.log(this.form.value);
+    console.log(this.form); //es el formGroup
+    console.log(this.form.valid);
+  }
 }
